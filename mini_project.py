1958860c3af0e20d9cd6caa1fd6419b567ee26d5
DiogoBernardoPT <diogobernardo888@hotmail.com>
2024-09-19 20:33:56 +0200

    Adding mini project.py

---
+++ mini_project.py
@@ -0,0 +1,16 @@
+import streamlit as st
+import plotly.express as px
+import numpy as np
+
+from modules.ml_func import *
+from eda import eda_app
+from ml import ml_app
+from about import about_app
+
+def main():
+    print('Hello first project')
+
+
+
+if __name__ == "__main__":
+    main()
